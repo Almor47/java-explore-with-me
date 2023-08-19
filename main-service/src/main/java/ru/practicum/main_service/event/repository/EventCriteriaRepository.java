be9dd73fd6d4b33567d863b276e473c4bbfdd159
Aleksandr <alex-moroz99@yandex.ru>
2023-08-19 17:22:28 +0300

    Чекстайл

--- main-service/src/main/java/ru/practicum/main_service/event/repository/EventCriteriaRepository.java
+++ main-service/src/main/java/ru/practicum/main_service/event/repository/EventCriteriaRepository.java
@@ -2,10 +2,8 @@ package ru.practicum.main_service.event.repository;
 
 import ru.practicum.main_service.event.dto.ParametersForAdminSearch;
 import ru.practicum.main_service.event.dto.ParametersForPublicSearch;
-import ru.practicum.main_service.event.enumerated.State;
 import ru.practicum.main_service.event.model.Event;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 public interface EventCriteriaRepository {
